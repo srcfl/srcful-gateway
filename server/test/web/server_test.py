3e7d1c5661e8ee4611b152068a96adecd4215458
Tobias Olsson <tobias.olsson@lnu.se>
2023-03-26 23:42:52 +0200

    more testing functions added

--- server/test/web/server_test.py
+++ server/test/web/server_test.py
@@ -1,14 +1,40 @@
-from ...web.server import Server
+from io import BytesIO
+from server.web.server import Server, requestHandlerFactory
+
 from http.server import HTTPServer
 
 
 def timeFunc():
-    return 0
+  return 0
 def getChipInfoFunc():
-    return "testymctestface"
+  return "testymctestface"
 
 def test_open_close():
-    s = Server(('localhost', 8081), {}, timeFunc, getChipInfoFunc)
-    assert isinstance(s._webServer, HTTPServer)
-    s.close()
-    assert s._webServer is None
+  s = Server(('localhost', 8081), {}, timeFunc, getChipInfoFunc)
+  assert isinstance(s._webServer, HTTPServer)
+  s.close()
+  assert s._webServer is None
+
+def test_handler_postBytest2Dict():
+  h = requestHandlerFactory(None, None, None)
+  d = h.post2Dict('')
+  assert  d == {}
+  assert h.post2Dict('foo') == {}
+  assert h.post2Dict('foo=') == {'foo': ''}
+  assert h.post2Dict('foo=bar') == {'foo': 'bar'}
+  assert h.post2Dict('foo=bar&baz=qux') == {'foo': 'bar', 'baz': 'qux'}
+  assert h.post2Dict('name=John%20Doe&age=30&location=New%20York') == {'name': 'John Doe', 'age': '30', 'location': 'New York'}
+
+def test_handler_getPostdata():
+  h = requestHandlerFactory(None, None, None)
+  assert h.getData({'Content-Length': '0'}, BytesIO(b'')) == {}
+  
+  data = b'name=John%20Doe&age=30&location=New%20York'
+  assert h.getData({'Content-Length': len(data)}, BytesIO(data)) == {'name': 'John Doe', 'age': '30', 'location': 'New York'}
+
+  data = b'{"name": "John Doe", "age": 30, "location": "New York"}'
+  assert h.getData({'Content-Length': len(data)}, BytesIO(data)) == {'name': 'John Doe', 'age': 30, 'location': 'New York'}
+
+  data = b'{"name": "John&Doe", "age": 30, "location": "New York = Kalmar"}'
+  assert h.getData({'Content-Length': len(data)}, BytesIO(data)) == {'name': 'John&Doe', 'age': 30, 'location': 'New York = Kalmar'}
+  
--- server/web/server.py
+++ server/web/server.py
@@ -1,17 +1,71 @@
+import json
 from typing import Callable
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import unquote_plus
 from .get import root
 
 
 def requestHandlerFactory(stats: dict, timeMSFunc:Callable, chipInfoFunc:Callable):
 
-  class MyHandler(BaseHTTPRequestHandler):
+  class Handler(BaseHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
-      super(MyHandler, self).__init__(*args, **kwargs)
+      super(Handler, self).__init__(*args, **kwargs)
+
+      self.api_get = {}
+      self.api_post = {}
+
+    @staticmethod
+    def post2Dict(post_data:str):
+      if "=" not in post_data:
+        return {}
+      return {unquote_plus(k): unquote_plus(v) for k, v in (x.split('=') for x in post_data.split('&'))}
+    
+    @staticmethod
+    def getAPIHandler(path:str, api_root:str, api_handler:dict):
+      if path.startswith(api_root):
+        handler = api_handler.get(path[len(api_root):])
+        if handler:
+          return handler
+      return None
+    
+    @staticmethod
+    def getData(headers:dict, rfile):
+      content_length = int(headers['Content-Length'])
+      content = rfile.read(content_length).decode("utf-8")
+
+      if content_length == 0 or len(content) == 0:
+        return {}
+
+      try:
+        post_data = json.loads(content)
+      except json.decoder.JSONDecodeError:
+        post_data = Handler.post2Dict(content)
+      except:
+        post_data = {}
+        
+      return post_data
+
+    def do_POST(self):
+      handler = Handler.getAPIHandler(self.path, "/api", self.api_post)
+      if handler is not None:
+        post_data = Handler.getData(self.headers, self.rfile)
+        
+        code, response = handler.doPost(post_data)
+        self.send_response(code)
+        self.send_header("Content-type", "application/json")
+        response = bytes(response, "utf-8")
+        self.send_header("Content-Length", len(response))
+        self.end_headers()
+        self.wfile.write(response)
+        return
+      else:
+        self.send_response(404)
+        self.end_headers()
+        return
 
     def do_GET(self):
 
-      htlm = root.Handler().doGet(self.stats, self.timeMSFunc, self.chipInfoFunc)
+      htlm = root.Handler().doGet(stats, timeMSFunc, chipInfoFunc)
       html_bytes = bytes(htlm, "utf-8")
 
       self.send_response(200)
@@ -22,7 +76,7 @@ def requestHandlerFactory(stats: dict, timeMSFunc:Callable, chipInfoFunc:Callabl
       self.wfile.write(html_bytes)
       
 
-  return MyHandler
+  return Handler
 
 class Server:
   _webServer:HTTPServer = None
