73a9dbb8264795fd5794b1c5b4e36fd5a901a4cf
Tobias Olsson <tobias.ohlsson@lnu.se>
2024-01-11 11:22:12 +0100

    removed nebra test

--- bluetooth/nebra/constants.py
+++
@@ -1,5 +0,0 @@
-g_service_uuid = "A07498CA-AD5B-474E-940D-16F1FBE7E8CD"
-g_request_char_uuid = "51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B" # for accepting requests ie. clients write to this
-g_response_char_uuid = "51FF12BB-3ED8-46E5-B4F9-D64E2FEC021C" # for sending responses ie client read from this
-
-g_api_url = "http://127.0.0.1"
