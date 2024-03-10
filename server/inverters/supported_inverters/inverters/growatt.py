820b8c9ddd84d37fe5889dadea29d911a69282ec
David M <dmozartandraws@gmail.com>
2024-03-11 00:15:37 +0100

    changing profiles

--- server/inverters/supported_inverters/inverters/growatt.py
+++ server/inverters/supported_inverters/inverters/growatt.py
@@ -8,6 +8,7 @@ profile = {
             RegistersKey.OPERATION: OperationKey.READ_INPUT_REGISTERS,
             RegistersKey.START_REGISTER: 0,
             RegistersKey.NUM_OF_REGISTERS: 92
-        }
+        },
+
     ]
 }
