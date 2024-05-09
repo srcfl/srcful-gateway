323c4a77a4e619b1cc6ad32c9ac4c90e07258f0c
David M <dmozartandraws@gmail.com>
2024-05-10 00:04:29 +0200

    endpoint for auto-scan for inverters on the network

--- server/web/handler/get/network.py
+++ server/web/handler/get/network.py
@@ -1,11 +1,11 @@
 import json
+import nmap
 import socket
-
 from server.wifi.wifi import get_connection_configs, is_connected, get_ip_address
-
 from ..handler import GetHandler
 from ..requestData import RequestData
 
+COMMON_INVERTER_PORTS = [502, 1502, 8877]
 
 class NetworkHandler(GetHandler):
     def schema(self):
@@ -33,3 +33,39 @@ class AddressHandler(GetHandler):
             return 200, json.dumps({"ip": get_ip_address(), "port": data.bb.rest_server_port})
         else:
             return 200, json.dumps({"ip": "no network", "port": 0})
+
+
+# A class to scan for modbus devices on the network
+class ModbusScanHandler(GetHandler):
+    def schema(self):
+        return self.create_schema(
+            "Scans the network for modbus devices",
+            returns={"devices": "a list of JSON Objects, each containing the host and port of a modbus device."}
+        )
+
+    def do_get(self, data: RequestData):
+
+        local_ip = get_ip_address()
+
+        # Extract the network prefix from the local IP address
+        network_prefix = '.'.join(local_ip.split('.')[:-1]) + '.0/24'
+
+        # Convert the list of common ports to a comma separated string for nmap
+        common_ports = ','.join(map(str, COMMON_INVERTER_PORTS)) 
+
+        nm = nmap.PortScanner()
+        nm.scan(network_prefix, ports=common_ports, arguments='-T5')
+
+        modbus_devices = []
+
+        for host in nm.all_hosts():
+            for port in COMMON_INVERTER_PORTS:
+                if nm[host].has_tcp(port) and nm[host]['tcp'][port]['state'] == 'open':
+                    device = {
+                    'host': host,
+                    'port': port
+                    }
+                    modbus_devices.append(device)
+    
+        return 200, json.dumps(modbus_devices)
+
