cfbad36f8382bf528e7f82de6e8d7ce6a6dbfab9
h0bb3 <tobias@srcful.io>
2024-05-14 22:41:13 +0200

    implemented single device connection

--- server/tasks/ModbusProxyTask.py
+++ server/tasks/ModbusProxyTask.py
@@ -23,12 +23,16 @@ class ModbusProxyTask(Task):
         self.server_socket.listen(5)
         self.server_socket.setblocking(False)  # Set non-blocking mode
 
+        # Setup single persistent connection to the target device
+        self.device_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.device_socket.connect((self.target_host, self.target_port))
+        self.device_socket.setblocking(False)
+
         # List of sockets to monitor
-        self.inputs = [self.server_socket]
+        self.inputs = [self.server_socket, self.device_socket]
         self.outputs = []
-        self.client_to_device = {}  # Map client sockets to device sockets
-        self.device_to_client = {}  # Map device sockets to client sockets
-        
+        self.client_to_device = {}  # Map client sockets to data buffers
+
     def execute(self, event_time):
         readable, writable, exceptional = select.select(self.inputs, self.outputs, self.inputs, 0.01)
 
@@ -38,38 +42,35 @@ class ModbusProxyTask(Task):
                 log.debug(f"New connection from {client_address}")
                 client_socket.setblocking(False)
                 self.inputs.append(client_socket)
+                self.client_to_device[client_socket] = b''
+            elif s is self.device_socket:
                 try:
-                    device_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                    device_socket.connect((self.target_host, self.target_port))
-                    device_socket.setblocking(False)
-                    self.inputs.append(device_socket)
-                    self.client_to_device[client_socket] = device_socket
-                    self.device_to_client[device_socket] = client_socket
+                    data = s.recv(1024)
+                    if data:
+                        log.debug(f"Forwarding data from device to client: {data.hex()}")
+                        for client_socket, client_data in self.client_to_device.items():
+                            client_socket.send(data)
+                    else:
+                        log.error("Device connection closed unexpectedly")
+                        self._close_socket(s)
                 except Exception as e:
-                    log.error(f"Could not connect to target device: {e}")
-                    self._close_socket(client_socket)
+                    log.error(f"Error receiving data from device: {e}")
             else:
                 try:
                     data = s.recv(1024)
                     if data:
-                        if s in self.client_to_device:
-                            #log.debug(f"Forwarding data from client to target device: {data.hex()}")
-                            device_socket = self.client_to_device[s]
-                            device_socket.send(data)
-                        elif s in self.device_to_client:
-                            #log.debug(f"Forwarding data from target device to client: {data.hex()}")
-                            client_socket = self.device_to_client[s]
-                            client_socket.send(data)
+                        log.debug(f"Forwarding data from client to target device: {data.hex()}")
+                        self.device_socket.send(data)
                     else:
                         self._close_socket(s)
                 except Exception as e:
                     self._close_socket(s)
-                    log.error(f"Error handling data: {e}")
+                    log.error(f"Error handling data from client: {e}")
 
         for s in exceptional:
             self._close_socket(s)
 
-        self.adjust_time(self.bb.time_ms() + 25)
+        self.adjust_time(event_time + 25)
         return self
 
     def _close_socket(self, s):
@@ -79,21 +80,5 @@ class ModbusProxyTask(Task):
         if s in self.outputs:
             self.outputs.remove(s)
         if s in self.client_to_device:
-            device_socket = self.client_to_device[s]
-            if device_socket in self.inputs:
-                self.inputs.remove(device_socket)
-            if device_socket in self.outputs:
-                self.outputs.remove(device_socket)
-            device_socket.close()
-            del self.device_to_client[device_socket]
             del self.client_to_device[s]
-        if s in self.device_to_client:
-            client_socket = self.device_to_client[s]
-            if client_socket in self.inputs:
-                self.inputs.remove(client_socket)
-            if client_socket in self.outputs:
-                self.outputs.remove(client_socket)
-            client_socket.close()
-            del self.client_to_device[client_socket]
-            del self.device_to_client[s]
         s.close()
