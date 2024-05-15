232d7a5139fe646d9d2536345400de54505e1402
David M <dmozartandraws@gmail.com>
2024-05-15 02:01:42 +0200

    port query param

--- server/web/handler/get/network.py
+++ server/web/handler/get/network.py
@@ -1,11 +1,11 @@
 import json
 import nmap
-import socket
 from server.wifi.wifi import get_connection_configs, is_connected, get_ip_address
 from ..handler import GetHandler
 from ..requestData import RequestData
+import logging
 
-COMMON_INVERTER_PORTS = [502, 1502, 8899, 48899]
+logger = logging.getLogger(__name__)
 
 class NetworkHandler(GetHandler):
     def schema(self):
@@ -44,22 +44,37 @@ class ModbusScanHandler(GetHandler):
         )
 
     def do_get(self, data: RequestData):
+        
+        parameters = data.query_params.get("ports", "")
+        if parameters == "":
+            return 400, json.dumps({"error": "missing ports parameter"})
+
+        parameters = parameters.split(",")
+        ports = []
+        for param in parameters:
+            if "-" in param:
+                interval = param.split("-")
+                ports += [*range(int(interval[0]), int(interval[1]) + 1)]
+            else:
+                ports.append(int(param))
+
+        # Convert the list of common ports to a comma separated string for nmap
+        comma_separated_ports = ','.join(map(str, ports))
+
+        logger.info(f"Scanning network for modbus devices on ports {comma_separated_ports}")
 
         local_ip = get_ip_address()
 
         # Extract the network prefix from the local IP address
         network_prefix = '.'.join(local_ip.split('.')[:-1]) + '.0/24'
 
-        # Convert the list of common ports to a comma separated string for nmap
-        common_ports = ','.join(map(str, COMMON_INVERTER_PORTS)) 
-
         nm = nmap.PortScanner()
-        nm.scan(network_prefix, ports=common_ports, arguments='-T5')
+        nm.scan(network_prefix, ports=comma_separated_ports, arguments='-T5')
 
         modbus_devices = []
 
         for host in nm.all_hosts():
-            for port in COMMON_INVERTER_PORTS:
+            for port in ports:
                 if nm[host].has_tcp(port) and nm[host]['tcp'][port]['state'] == 'open':
                     device = {
                     'host': host,
