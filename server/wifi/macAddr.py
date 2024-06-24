bca893239a7976790e98c865e8904577464f9128
h0bb3 <tobias.ohlsson@gmail.com>
2024-06-24 22:09:31 +0200

    went with dbus network manager for eth0 mac also

--- server/wifi/macAddr.py
+++ server/wifi/macAddr.py
@@ -1,26 +1,33 @@
 import sys
 
 
+import sys
+import dbus
+import uuid
+
+class NetworkDeviceNotFoundError(Exception):
+    pass
+
+def get_device_mac_address(device_type):
+    bus = dbus.SystemBus()
+    manager = dbus.Interface(bus.get_object('org.freedesktop.NetworkManager', '/org/freedesktop/NetworkManager'), 'org.freedesktop.NetworkManager')
+    for path in manager.GetDevices():
+        dev_proxy = bus.get_object('org.freedesktop.NetworkManager', path)
+        iface = dbus.Interface(dev_proxy, 'org.freedesktop.DBus.Properties')
+        properties = iface.GetAll('org.freedesktop.NetworkManager.Device')
+        if properties['DeviceType'] == device_type:
+            device_properties = iface.GetAll('org.freedesktop.NetworkManager.Device')
+            return device_properties['HwAddress']
+    raise NetworkDeviceNotFoundError(f"No device found with DeviceType {device_type}")
+
 def get_eth0():
     if sys.platform == 'linux':
-        return open('/sys/class/net/eth0/address').readline().strip().upper()
+        return get_device_mac_address(1)  # 1 is for ethernet
     else:
-        import uuid
-        return ':'.join(['{:02x}'.format((uuid.getnode() >> ele) & 0xff) for ele in range(0,8*6,8)][::-1])
-
+        return ':'.join(['{:02x}'.format((uuid.getnode() >> ele) & 0xff) for ele in range(0, 8*6, 8)][::-1])
 
 def get_wlan0():
     if sys.platform == 'linux':
-        import dbus
-        bus = dbus.SystemBus()
-        manager = dbus.Interface(bus.get_object('org.freedesktop.NetworkManager', '/org/freedesktop/NetworkManager'), 'org.freedesktop.NetworkManager')
-        for path in manager.GetDevices():
-            dev_proxy = bus.get_object('org.freedesktop.NetworkManager', path)
-            iface = dbus.Interface(dev_proxy, 'org.freedesktop.DBus.Properties')
-            properties = iface.GetAll('org.freedesktop.NetworkManager.Device')
-            if properties['DeviceType'] == 2:  # 2 is for wifi
-                wifi_iface = dbus.Interface(dev_proxy, 'org.freedesktop.NetworkManager.Device.Wireless')
-                wifi_properties = iface.GetAll('org.freedesktop.NetworkManager.Device.Wireless')
-                return wifi_properties['HwAddress']
+        return get_device_mac_address(2)  # 2 is for wifi
     else:
         raise NotImplementedError("Only implemented for linux platform")
