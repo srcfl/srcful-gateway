3b5803e01bc8def8b9e558f65a4193178d7b5b4d
David M <dmozartandraws@gmail.com>
2024-09-05 09:58:49 +0200

    old device creation endpoints are now using the new factory

--- server/web/handler/post/modbusRTU.py
+++ server/web/handler/post/modbusRTU.py
@@ -1,11 +1,11 @@
 import json
-from server.inverters.ModbusRTU import ModbusRTU
 from server.tasks.openDeviceTask import OpenDeviceTask
-
 from ..handler import PostHandler
 from ..requestData import RequestData
-
 import logging
+from server.inverters.IComFactory import IComFactory
+from server.inverters.ICom import ICom
+from server.inverters.der import DER
 
 logger = logging.getLogger(__name__)
 
@@ -34,25 +34,19 @@ class Handler(PostHandler):
         return json.dumps(self.schema())
 
     def do_post(self, data: RequestData) -> tuple[int, str]:
-        if "port" in data.data and "type" in data.data:
-            try:
-                conf = (
-                    data.data["port"],
-                    int(data.data["baudrate"]),
-                    int(data.data["bytesize"]),
-                    data.data["parity"],
-                    float(data.data["stopbits"]),
-                    data.data["type"],
-                    int(data.data["address"]),
-                )
-                inverter = ModbusRTU(conf)
-                logger.info("Created an RTU inverter")
-
-                data.bb.add_task(OpenDeviceTask(data.bb.time_ms() + 100, data.bb, inverter))
-                return 200, json.dumps({"status": "ok"})
-            except Exception as e:
-                logger.error("Failed to open inverter: %s", data.data)
-                logger.error(e)
-                return 500, json.dumps({"status": "error", "message": str(e)})
-        else:
-            return 400, json.dumps({"status": "bad request"})
+        try:
+            if ICom.CONNECTION_KEY not in data.data:
+                return 400, json.dumps({"status": "connection field is required"})
+            
+            conf = IComFactory.parse_connection_config_from_dict(data.data)
+            com = IComFactory.create_com(conf)
+            logger.info(f"Created a Modbus {conf[0]} connection")
+            der = DER(com)
+            
+            data.bb.add_task(OpenDeviceTask(data.bb.time_ms() + 100, data.bb, der))
+            return 200, json.dumps({"status": "ok"})    
+            
+        except Exception as e:
+            logger.error(f"Failed to open a Modbus {conf[0]} connection: {conf}")
+            logger.error(e)
+            return 500, json.dumps({"status": "error", "message": str(e)})
--- server/web/handler/post/modbusSolarman.py
+++ server/web/handler/post/modbusSolarman.py
@@ -1,10 +1,11 @@
 import json
-from server.inverters.ModbusSolarman import ModbusSolarman
 from server.tasks.openDeviceTask import OpenDeviceTask
 from ..handler import PostHandler
 from ..requestData import RequestData
-
 import logging
+from server.inverters.IComFactory import IComFactory
+from server.inverters.ICom import ICom
+from server.inverters.der import DER
 
 logger = logging.getLogger(__name__)
 
@@ -27,30 +28,19 @@ class Handler(PostHandler):
         )
 
     def do_post(self, data: RequestData) -> tuple[int, str]:
-        if (
-            "ip" in data.data
-            and "serial" in data.data
-            and "port" in data.data
-            and "type" in data.data
-            and "address" in data.data
-        ):
-            try:
-                conf = (
-                    data.data["ip"],
-                    int(data.data["serial"]),
-                    int(data.data["port"]),
-                    data.data["type"],
-                    int(data.data["address"]),
-                    0
-                )
-                inverter = ModbusSolarman(conf) 
-                logger.info("Created a SolarmanV5 inverter")
-
-                data.bb.add_task(OpenDeviceTask(data.bb.time_ms() + 100, data.bb, inverter))
-                return 200, json.dumps({"status": "ok"})
-            except Exception as e:
-                logger.error("Failed to open inverter: %s", data.data)
-                logger.error(e)
-                return 500, json.dumps({"status": "error", "message": str(e)})
-        else:
-            return 400, json.dumps({"status": "bad request"})
+        try:
+            if ICom.CONNECTION_KEY not in data.data:
+                return 400, json.dumps({"status": "connection field is required"})
+            
+            conf = IComFactory.parse_connection_config_from_dict(data.data)
+            com = IComFactory.create_com(conf)
+            logger.info(f"Created a Modbus {conf[0]} connection")
+            der = DER(com)
+            
+            data.bb.add_task(OpenDeviceTask(data.bb.time_ms() + 100, data.bb, der))
+            return 200, json.dumps({"status": "ok"})    
+            
+        except Exception as e:
+            logger.error(f"Failed to open a Modbus {conf[0]} connection: {conf}")
+            logger.error(e)
+            return 500, json.dumps({"status": "error", "message": str(e)})
