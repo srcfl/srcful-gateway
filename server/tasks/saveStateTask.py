5db63e895f1f0eacf2edfdb3d1e7d4b179419d48
h0bb3 <tobias.ohlsson@gmail.com>
2024-09-11 10:05:02 +0200

    some more logging in the save state task

--- server/tasks/saveStateTask.py
+++ server/tasks/saveStateTask.py
@@ -9,22 +9,27 @@ logger.setLevel(logging.INFO)
 
 class SaveStateTask(ConfigurationMutationTask):
     SUBKEY = "state"
-    def __init__(self, event_time: int, bb: BlackBoard, state: dict = None):
-        self.state = state if state is not None else bb.state
+    def __init__(self, event_time: int, bb: BlackBoard):
+        self.state = bb.state
         super().__init__(event_time, bb, self.SUBKEY, self.state)
+        logger.info("State: %s", self.state)
+
 
     def _on_200(self, reply):
         super()._on_200(reply)
 
 
     def _on_error(self, reply):
+        logger.error("Failed to save state %s", reply)
+        logger.error("State: %s", self.state)
         super()._on_error(reply)
+        self.state = self.bb.state
         return 60000
     
     
 class SaveStatePerpetualTask(SaveStateTask  ):
-    def __init__(self, event_time: int, bb: BlackBoard, state: dict = None):
-        super().__init__(event_time, bb, state)
+    def __init__(self, event_time: int, bb: BlackBoard):
+        super().__init__(event_time, bb)
 
     def _on_200(self, reply):
         super()._on_200(reply)
@@ -33,5 +38,8 @@ class SaveStatePerpetualTask(SaveStateTask  ):
 
     def _on_error(self, reply):
         super()._on_error(reply)
+        logger.error("Failed to save state %s", reply)
+        logger.error("State: %s", self.state)
         logger.warning("Failed to save state %s", reply)
+        self.state = self.bb.state
         return 1000 * 10 # Retry after 10 seconds
