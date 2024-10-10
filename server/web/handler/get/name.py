5bcfdcf72679263da7bc8f341b3d2d740a3c4810
David M <dmozartandraws@gmail.com>
2024-10-10 14:04:59 +0200

    /api/name should now return 504 when the gateway is offline

--- server/web/handler/get/name.py
+++ server/web/handler/get/name.py
@@ -21,6 +21,12 @@ class Handler(GetHandler):
         t.execute(0)
 
         if t.name is None:
-            return t.reply.status, json.dumps({"body": t.reply.body})
+            # Then the output can also be {"exception": "'Response' object has no attribute 'status'", "endpoint": "/api/name"} 
+            # If the gateway is offline and can not reach the endpoint
+            # So we need to return a 504 error
+            if hasattr(t.reply, 'status'):
+                return t.reply.status, json.dumps({"body": t.reply.body})
+            else:
+                return 504, json.dumps({"body": t.reply})
 
         return 200, json.dumps({"name": t.name})
