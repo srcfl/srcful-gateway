ee6c71a1c44c3e8207ed16e8c5445495acd034df
h0bb3 <tobias.ohlsson@gmail.com>
2024-10-21 11:14:18 +0200

    added mdns to the network stack using zeroconf, some more p1 test cases

--- server/devices/P1Telnet.py
+++ server/devices/P1Telnet.py
@@ -1,6 +1,9 @@
 import logging
 import telnetlib
+from typing import Callable, List
 from pymodbus.exceptions import ConnectionException, ModbusException, ModbusIOException
+
+from server.network import mdns as mdns
 from .supported_inverters.profiles import InverterProfiles, InverterProfile
 from .ICom import HarvestDataType, ICom
 from server.network.network_utils import NetworkUtils
@@ -24,8 +27,12 @@ class P1Telnet(ICom):
         self.id = id
 
     def connect(self) -> bool:
+        return self._connect(telnetlib.Telnet)
+        
+    
+    def _connect(self, telnet_factory: Callable[[str, int, int], telnetlib.Telnet]) -> bool:
         try:
-            self.client = telnetlib.Telnet(self.ip, self.port, timeout=5)
+            self.client = telnet_factory(self.ip, self.port, 5)
             logger.info(f"Successfully connected to {self.ip}:{self.port}")
             harvest = self.read_harvest_data(False)
             if self.id == "":
@@ -37,6 +44,7 @@ class P1Telnet(ICom):
         except Exception as e:
             logger.error(f"Failed to connect to {self.ip}:{self.port}: {str(e)}")
             return False
+        
     
     def is_valid(self) -> bool:
         return self.id != ""
@@ -54,19 +62,29 @@ class P1Telnet(ICom):
     
     def read_harvest_data(self, force_verbose) -> dict:
         try:
-            # Read until the start of a P1 message
-            self.client.read_until(b"/", timeout=17)
-            
-            # Read the entire P1 message
-            p1_message = self.client.read_until(b"!", timeout=17).decode('ascii')
-            
+            p1_message = self._read_harvest_data(self.client)
+
             # Parse the P1 message
             data = self._parse_p1_message(p1_message)
             
             return data
         except Exception as e:
             logger.error(f"Error reading P1 data: {str(e)}")
-            return {}
+            raise
+        
+    def _read_harvest_data(self, telnet_client: telnetlib.Telnet) -> str:
+        # Read until the start of a P1 message
+        telnet_client.read_until(b"/", timeout=17)
+        
+        # Read the entire P1 message including the last crc check
+        p1_message = "/" + telnet_client.read_until(b"!", timeout=17).decode('ascii') + telnet_client.read_until(b"\r\n", timeout=17).decode('ascii')
+
+        if len(p1_message) < 5:
+            logger.error(f"P1 message is too short: {p1_message}")
+            raise Exception(f"P1 message is too short: {p1_message}")
+        
+        return p1_message
+            
 
     def _parse_p1_message(self, message: str) -> dict:
         lines = message.strip().split()
@@ -75,8 +93,8 @@ class P1Telnet(ICom):
             'rows': []
         }
         
-        # Extract serial number from the first line
-        data['serial_number'] = lines[0]  # Remove the leading '/'
+        # Extract serial number from the first line remove the leading '/'
+        data['serial_number'] = lines[0][1:]
         
         # Add all other lines to the rows list
         data['rows'] = [line.strip() for line in lines[1:] if line.strip()]
@@ -101,7 +119,15 @@ class P1Telnet(ICom):
         return P1Telnet(ip, self.port, self.id)
     
     def find_device(self) -> 'ICom':
-        raise NotImplementedError("find_device is not implemented for P1Telnet")
+        """ If there is an id we try to find a device with that id, using multicast dns for for supported devices"""
+        if self.id:
+            mdns_services: List[mdns.ServiceResult] = mdns.scan(5, "_currently._tcp.")
+            for service in mdns_services:
+                if service.address and service.port:
+                    p1 = P1Telnet(service.address, service.port, self.id)
+                    if p1.connect():
+                        return p1
+        return None
     
     def get_SN(self) -> str:
         return self.id
--- server/tests/server_unit_test/unit_tests/inverters/p1telnet_test.py
+++ server/tests/server_unit_test/unit_tests/inverters/p1telnet_test.py
@@ -1,7 +1,44 @@
+from io import BytesIO
 import pytest 
 
 from server.devices.P1Telnet import P1Telnet
 
+
+
+        
+
+class DataReader:
+    def __init__(self, data):
+        self.data = BytesIO(data.encode('ascii'))
+
+    def read_until(self, delimiter, timeout):
+        result = b''
+        while True:
+            char = self.data.read(1)
+            if not char:
+                break
+            result += char
+            if char == delimiter:
+                break
+        return result
+    
+    def close(self):
+        pass
+
+    def get_socket(self):
+        return "test_socket"
+    
+class MockTelnet:
+    def __init__(self, host, port, timeout):
+        self.host = host
+        self.port = port
+        self.timeout = timeout
+
+    def connect(self):
+        return True
+    
+    def read_until(self, delimiter, timeout):
+        return DataReader(self.data).read_until(delimiter, timeout)
 
 # set to test_ to try real harvesting
 def real_harvest():
@@ -15,3 +52,57 @@ def real_harvest():
     assert harvest is not None
     assert harvest['serial_number'] is not None
     assert len(harvest['rows']) > 0
+
+
+def test_parse_p1_data():
+    p1 = P1Telnet("192.168.0.30", 23, "")
+    harvest = p1._parse_p1_message(get_p1_data())
+    assert harvest is not None
+    assert harvest['serial_number'] == "LGF5E360"
+    assert len(harvest['rows']) == 28
+    assert harvest['rows'][-1] == '!A267'
+
+def test_read_harvest_data():
+    mock_telnet = DataReader(get_p1_data())
+    p1 = P1Telnet("192.168.0.30", 23, "")
+    harvest = p1._read_harvest_data(mock_telnet)
+    assert '/LGF5E360' in harvest
+    assert '!A267' in harvest
+
+    
+
+def get_p1_data():
+
+    return """
+/LGF5E360
+
+0-0:1.0.0(241015201510W)
+1-0:1.8.0(00007225.264kWh)
+1-0:2.8.0(00000000.000kWh)
+1-0:3.8.0(00000003.885kVArh)
+1-0:4.8.0(00001466.506kVArh)
+1-0:1.7.0(0000.716kW)
+1-0:2.7.0(0000.000kW)
+1-0:3.7.0(0000.000kVAr)
+1-0:4.7.0(0000.179kVAr)
+1-0:21.7.0(0000.071kW)
+1-0:22.7.0(0000.000kW)
+1-0:41.7.0(0000.636kW)
+1-0:42.7.0(0000.000kW)
+1-0:61.7.0(0000.008kW)
+1-0:62.7.0(0000.000kW)
+1-0:23.7.0(0000.000kVAr)
+1-0:24.7.0(0000.062kVAr)
+1-0:43.7.0(0000.000kVAr)
+1-0:44.7.0(0000.100kVAr)
+1-0:63.7.0(0000.000kVAr)
+1-0:64.7.0(0000.016kVAr)
+1-0:32.7.0(234.3V)
+1-0:52.7.0(233.6V)
+1-0:72.7.0(234.7V)
+1-0:31.7.0(000.4A)
+1-0:51.7.0(002.7A)
+1-0:71.7.0(000.0A)
+!A267
+"""
+
