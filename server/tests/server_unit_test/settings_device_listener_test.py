82022c2584cb61e982a9876a31f868c6b58c60e0
David M <dmozartandraws@gmail.com>
2024-11-05 11:17:55 +0100

    moved testcases and added a new test case for device settings

--- server/tests/server_unit_test/settings_device_listener_test.py
+++ server/tests/server_unit_test/settings_device_listener_test.py
@@ -1,10 +1,12 @@
 from server.settings_device_listener import SettingsDeviceListener
+from server.tasks.harvestFactory import HarvestFactory
 from server.tasks.openDevicePerpetualTask import DevicePerpetualTask
 from server.blackboard import BlackBoard
 from server.settings import ChangeSource
 from server.network.network_utils import NetworkUtils
 import pytest 
-from unittest.mock import patch, MagicMock
+from unittest.mock import MagicMock
+from server.tasks.saveStateTask import SaveStateTask
 
 
 @pytest.fixture
@@ -15,6 +17,7 @@ def blackboard():
 def settings_device_listener(blackboard):
     return SettingsDeviceListener(blackboard)
 
+# @pytest.fixture
 def assert_initial_state(settings_device_listener):
     """Helper function to assert the initial state of connections."""
     assert settings_device_listener.blackboard.settings.devices._connections == []
@@ -92,7 +95,6 @@ def test_add_duplicate_connections(settings_device_listener):
         {
             "host": "192.168.0.240",
             "port": 5002,
-            "mac": "00:00:00:00:00:00",
             "type": "sma",
             "address": 1,
             "connection": "TCP"
@@ -100,7 +102,6 @@ def test_add_duplicate_connections(settings_device_listener):
         {
             "host": "192.168.0.240",
             "port": 5002,
-            "mac": "00:00:00:00:00:00",
             "type": "sma",
             "address": 2,
             "connection": "TCP"
@@ -123,7 +124,6 @@ def test_settings_updated_after_device_opened_with_old_format(settings_device_li
         {
             "host": "192.168.0.240",
             "port": 5002,
-            "mac": "00:00:00:00:00:00",
             "type": "sma",
             "address": 1,
             "connection": "TCP"
@@ -137,6 +137,7 @@ def test_settings_updated_after_device_opened_with_old_format(settings_device_li
     assert isinstance(task, DevicePerpetualTask)
     
     mock_device_methods(task)
+    
     task.execute(0)
     
     assert len(settings_device_listener.blackboard.devices.lst) == 1
@@ -146,3 +147,47 @@ def test_settings_updated_after_device_opened_with_old_format(settings_device_li
     assert settings_device_listener.blackboard.devices.contains(task.device)
     
     assert conf == settings_device_listener.blackboard.devices.lst[0].get_config()
+    
+
+def test_fetch_device_settings_in_old_format(settings_device_listener):
+    # HarvestFactory(settings_device_listener.blackboard)
+    settings_device_listener.blackboard.settings.devices._connections = [
+        {
+            "host": "192.168.0.240",
+            "port": 5002,
+            "type": "sma",
+            "mac": NetworkUtils.INVALID_MAC,
+            "slave_id": 1,
+            "connection": "TCP"
+        }
+    ]
+    settings_device_listener._perform_action(ChangeSource.BACKEND) # _connection gets updated at this point to conform to the new format
+    
+    task = settings_device_listener.blackboard._tasks[0]
+    
+    assert isinstance(task, DevicePerpetualTask)
+    
+    mock_device_methods(task)
+    
+    task = settings_device_listener.blackboard.purge_tasks()
+    
+    task = task[0]
+    
+    def mock_connect():
+        task.device.mac = "00:00:00:00:00:02"  # Set "real" MAC
+        return True
+    
+    task.device.connect = mock_connect
+    
+    task = task.execute(0)
+    
+    assert task is None # Device is opened (mocked above in mock_device_methods), so this should return None
+    
+    assert len(settings_device_listener.blackboard.devices.lst) == 1
+    
+    assert len(settings_device_listener.blackboard.settings.devices._connections) == 1
+    
+    new_config = settings_device_listener.blackboard.settings.devices._connections[0]
+    
+    # Assert that the device is updated in settings
+    assert new_config == settings_device_listener.blackboard.devices.lst[0].get_config() # this should break, the MAC is not updated in the settings
