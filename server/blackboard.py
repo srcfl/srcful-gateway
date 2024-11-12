09cdbb1b470c401391613e0ace61ae91fccf586a
David M <dmozartandraws@gmail.com>
2024-11-12 10:34:10 +0100

    0.16.4

--- server/blackboard.py
+++ server/blackboard.py
@@ -215,7 +215,7 @@ class BlackBoard:
         return (time.monotonic_ns() - self._start_time) // 1_000_000
 
     def get_version(self) -> str:
-        return "0.16.3"
+        return "0.16.4"
 
     def get_chip_info(self):
         with crypto.Chip() as chip:
