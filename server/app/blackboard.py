c2cc2db3c3da9c2fa427ab3b9c7e9fef3812a5e1
David M <dmozartandraws@gmail.com>
2024-11-19 13:52:19 +0100

    0.16.6 - fix LoRa beacon band for US

--- server/app/blackboard.py
+++ server/app/blackboard.py
@@ -49,7 +49,7 @@ class BlackBoard(ISystemTime, ITaskSource):
         self._available_devices = []
         
     def get_version(self) -> str:
-        return "0.16.5"
+        return "0.16.6"
         
     def add_task(self, task: ITask):
         self._tasks.append(task)
