ae8793355d9b02903e88e01c008d8da35e0ac3e0
h0bb3 <tobias.ohlsson@gmail.com>
2024-12-18 15:05:18 +0100

    delay before gql restart

--- server/tasks/openWiFiConTask.py
+++ server/tasks/openWiFiConTask.py
@@ -1,4 +1,5 @@
 import logging
+import time
 from server.network.wifi import WiFiHandler
 from server.app.blackboard import BlackBoard
 from server.tasks.scanWiFiTask import ScanWiFiTask
@@ -20,19 +21,22 @@ class OpenWiFiConTask(Task):
             self.wificon.connect()
             self.bb.add_task(ScanWiFiTask(self.bb.time_ms() + 1000, self.bb))
 
+            # we now sleep for 30 seconds to give the graphql client a chance to reconnect
+            time.sleep(30)
+            # this is a temporary fix to get the graphql client to work when disconnected and reconnected with internet
+            log.info("Stopping graphql client")
+            from server.web.socket.settings_subscription import GraphQLSubscriptionClient
+
+            self.bb.graphql_client.stop()
+            log.info("Starting graphql client")
+            self.bb.graphql_client = GraphQLSubscriptionClient(self.bb, "wss://api.srcful.dev/")
+            self.bb.graphql_client.start()
+            log.info("Graphql client started")
+
             return None
         except Exception as e:
             self.bb.notify_error("Failed to connect to WiFi. Invalid SSID or PSK: ")
             log.exception(e)
             self.time = event_time + 10000
             return self
-        finally:
-            # this is a temporary fix to get the graphql client to work when disconnected and reconnected with internet
-            from server.web.socket.settings_subscription import GraphQLSubscriptionClient
-
-            self.bb.graphql_client.stop()
-            graphql_client = GraphQLSubscriptionClient(self.bb, "wss://api.srcful.dev/")
-            graphql_client.start()
-
-            self.bb.graphql_client = graphql_client
 
