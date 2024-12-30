e9567e79e1d76a2e7d7092bb2038c776d8958784
David M <dmozartandraws@gmail.com>
2024-12-30 09:35:57 +0100

    update goodwe definitions

--- server/devices/supported_devices/supported_devices.py
+++ server/devices/supported_devices/supported_devices.py
@@ -543,14 +543,39 @@ supported_devices = {
             ProfileKey.REGISTERS_VERBOSE: [
                 {
                     RegistersKey.FUNCTION_CODE: FunctionCodeKey.READ_HOLDING_REGISTERS,
-                    RegistersKey.START_REGISTER: 1280,
-                    RegistersKey.NUM_OF_REGISTERS: 68,
+                    RegistersKey.START_REGISTER: 35000,
+                    RegistersKey.NUM_OF_REGISTERS: 40,
+                },
+                {
+                    RegistersKey.FUNCTION_CODE: FunctionCodeKey.READ_HOLDING_REGISTERS,
+                    RegistersKey.START_REGISTER: 35100,
+                    RegistersKey.NUM_OF_REGISTERS: 40,
+                },
+                {
+                    RegistersKey.FUNCTION_CODE: FunctionCodeKey.READ_HOLDING_REGISTERS,
+                    RegistersKey.START_REGISTER: 35141,
+                    RegistersKey.NUM_OF_REGISTERS: 40,
+                },
+                {
+                    RegistersKey.FUNCTION_CODE: FunctionCodeKey.READ_HOLDING_REGISTERS,
+                    RegistersKey.START_REGISTER: 35182,
+                    RegistersKey.NUM_OF_REGISTERS: 40,
+                },
+                {
+                    RegistersKey.FUNCTION_CODE: FunctionCodeKey.READ_HOLDING_REGISTERS,
+                    RegistersKey.START_REGISTER: 36000,
+                    RegistersKey.NUM_OF_REGISTERS: 44,
+                },
+                {
+                    RegistersKey.FUNCTION_CODE: FunctionCodeKey.READ_HOLDING_REGISTERS,
+                    RegistersKey.START_REGISTER: 37000,
+                    RegistersKey.NUM_OF_REGISTERS: 23,
                 }
             ],
             ProfileKey.REGISTERS: [
                 {
                     RegistersKey.FUNCTION_CODE: FunctionCodeKey.READ_HOLDING_REGISTERS,
-                    RegistersKey.START_REGISTER: 1305,
+                    RegistersKey.START_REGISTER: 35123,
                     RegistersKey.NUM_OF_REGISTERS: 1,
                     RegistersKey.DATA_TYPE: DataTypeKey.U16,
                     RegistersKey.UNIT: "Hz",
@@ -560,16 +585,16 @@ supported_devices = {
                 },
                 {
                     RegistersKey.FUNCTION_CODE: FunctionCodeKey.READ_HOLDING_REGISTERS,
-                    RegistersKey.START_REGISTER: 1280,
-                    RegistersKey.NUM_OF_REGISTERS: 2, # PV1. First register is the voltage, second is the current
-                    RegistersKey.DATA_TYPE: DataTypeKey.U16,
+                    RegistersKey.START_REGISTER: 35138,
+                    RegistersKey.NUM_OF_REGISTERS: 1, # PV1. First register is the voltage, second is the current
+                    RegistersKey.DATA_TYPE: DataTypeKey.I16,
                     RegistersKey.UNIT: "W",
                     RegistersKey.DESCRIPTION: "PV 1 Power",
                     RegistersKey.SCALE_FACTOR: 0.1,
                     RegistersKey.ENDIANNESS: EndiannessKey.BIG,
                 },
             ],
-            ProfileKey.KEYWORDS: ["high-flying"],
+            ProfileKey.KEYWORDS: ["goodwe", "high-flying"],
         },
         {
             ProfileKey.NAME: "ferroamp",
