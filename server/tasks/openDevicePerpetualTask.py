adf0d19dfc9300136a95a1aadf9f77f6ee3baf1e
David M <dmozartandraws@gmail.com>
2025-01-09 10:37:08 +0100

    fixes #310

--- server/tasks/openDevicePerpetualTask.py
+++ server/tasks/openDevicePerpetualTask.py
@@ -8,12 +8,12 @@ from server.network.network_utils import NetworkUtils
 
 logger = logging.getLogger(__name__)
 
+
 class DevicePerpetualTask(Task):
     def __init__(self, event_time: int, bb: BlackBoard, device: ICom):
         super().__init__(event_time, bb)
-        self.device:ICom = device
+        self.device: ICom = device
         # self.old_device: Optional[ICom] = None
-
 
     def in_settings(self, device: ICom):
         for settings in self.bb.settings.devices.connections:
@@ -26,21 +26,21 @@ class DevicePerpetualTask(Task):
         logger.info("*************************************************************")
         logger.info("******************** DevicePerpetualTask ********************")
         logger.info("*************************************************************")
-        
+
         # Check if the device is already in the blackboard and is open
-        # If it is, we don't need to do anything. This check is necessary to ensure 
+        # If it is, we don't need to do anything. This check is necessary to ensure
         # that a device does not get opened multiple times.
         existing_device = self.bb.devices.find_sn(self.device.get_SN())
         if existing_device and existing_device.is_open():
             return None
-        
+
         # If the device is not in the settings list it has probably been closed so lets not do anything
         if not self.in_settings(self.device):
             return None
-        
+
         try:
             if self.device.connect():
-                
+
                 if self.bb.devices.contains(self.device) and not self.device.is_open():
                     message = "Device is already in the blackboard, no action needed"
                     logger.error(message)
@@ -52,19 +52,19 @@ class DevicePerpetualTask(Task):
 
                 # if self.old_device:
                 #     self.bb.settings.devices.remove_connection(self.old_device, ChangeSource.LOCAL)
-                
+
                 self.bb.devices.add(self.device)
                 return None
-            
+
             else:
-                
+
                 # if self.old_device:
                 #     logger.info("Device not found in two steps, giving up the perpetual task: %s, %s", self.old_device.get_SN(), self.device.get_SN())
                 #     logger.info("Removing device from settings: %s", self.old_device.get_SN())
                 #     self.bb.settings.devices.remove_connection(self.old_device, ChangeSource.LOCAL)
 
-                tmp_device = self.device.find_device() # find the device on the network this is the same device but the method of connection (e.g. ip can be different)
-                               
+                tmp_device = self.device.find_device()  # find the device on the network this is the same device but the method of connection (e.g. ip can be different)
+
                 if tmp_device is not None:
 
                     # self.old_device = self.device
@@ -77,9 +77,9 @@ class DevicePerpetualTask(Task):
                     logger.info(message)
                     self.bb.add_error(message)
                     self.time = event_time + 60000 * 5
-                    
+
             return self
-        
+
         except Exception as e:
             logger.exception("Exception opening a device: %s", e)
             self.time = event_time + 10000
