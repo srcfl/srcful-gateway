9a75ac09ac7a07b067b5ad36dfb1ac10450eaf26
David M <dmozartandraws@gmail.com>
2025-02-10 17:16:54 +0100

    fixes #377

--- server/app/blackboard.py
+++ server/app/blackboard.py
@@ -49,7 +49,7 @@ class BlackBoard(ISystemTime, ITaskSource):
         self._available_devices = []
 
     def get_version(self) -> str:
-        return "0.18.17"
+        return "0.18.19"
 
     def add_task(self, task: ITask):
         self._tasks.append(task)
