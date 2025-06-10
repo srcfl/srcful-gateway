ed53e25ff839c678a77fec38e1c01aaa08aa9adc
h0bb3 <tobias.ohlsson@gmail.com>
2025-06-10 17:22:01 +0200

    added harvest transport to powerLimitControllerTask and some refacoring

--- server/tasks/harvest.py
+++ server/tasks/harvest.py
@@ -6,7 +6,7 @@ from server.tasks.itask import ITask
 from server.tasks.openDevicePerpetualTask import DevicePerpetualTask
 from server.app.blackboard import BlackBoard
 
-from .task import Task
+from .harvestableTask import HarvestableTask
 from .harvestTransport import ITransportFactory
 from server.devices.ICom import ICom
 
@@ -14,19 +14,10 @@ logger = logging.getLogger(__name__)
 logger.setLevel(level=logging.DEBUG)
 
 
-class Harvest(Task):
+class Harvest(HarvestableTask):
     def __init__(self, event_time: int, bb: BlackBoard, device: ICom, transport_factory: ITransportFactory):
-        super().__init__(event_time, bb)
-        self.device = device
-        self.barn: dict[int, dict] = {}
+        super().__init__(event_time, bb, device, transport_factory)
         self.backoff_time = 1000  # start with a 1000ms backoff
-        self.transport_factory = transport_factory
-        self.last_transport_time = bb.time_ms()
-        self.harvest_count = 0
-        self.total_harvest_time_ms = 0
-        self.packet_count = 0
-        self.data_points_count = 0
-        self.packet_counter_timestamp = event_time
 
     def execute(self, event_time) -> Union[List[ITask], ITask, None]:
 
@@ -53,23 +44,15 @@ class Harvest(Task):
 
         try:
 
-            if self.device.get_mode() == DeviceMode.CONTROL:
+            if hasattr(self.device, 'get_mode') and self.device.get_mode() == DeviceMode.CONTROL:
                 logger.info("Device is in control mode, going to power limit controller task")
                 from server.tasks.powerLimitControllerTask import PowerLimitControllerTask
                 return PowerLimitControllerTask(self.bb.time_ms() + 1000, self.bb, self.device)
 
-
-            harvest = self.device.read_harvest_data(force_verbose=self.harvest_count % 10 == 0)
-            self.harvest_count += 1
-            end_time = self.bb.time_ms()
-
-            elapsed_time_ms = end_time - start_time
+            harvest = self.collect_harvest_data(start_time)
+            elapsed_time_ms = self.bb.time_ms() - start_time
             self.backoff_time = self.device.get_backoff_time_ms(elapsed_time_ms, self.backoff_time)
             logger.debug("Harvest from [%s] took %s ms. Data points: %s", self.device.get_SN(), elapsed_time_ms, len(harvest))
-
-            self.total_harvest_time_ms += elapsed_time_ms
-
-            self.barn[end_time] = harvest
 
         except Exception as e:
 
@@ -95,42 +78,3 @@ class Harvest(Task):
         if len(transport) > 0:
             return [self] + transport
         return self
-
-    def _create_headers(self, device: ICom) -> dict:
-        headers = {"model": ""}
-        headers["dtype"] = device.get_harvest_data_type().value
-        headers["sn"] = device.get_SN()
-        headers["model"] = device.get_name().lower()
-        return headers
-
-    def _create_transport(self, event_time: int, endpoints: list[str], force_transport: bool = False) -> List[ITask]:
-        ret: List[ITask] = []
-
-        if not force_transport:
-            # check if the lowest time in the barn is more than 10 seconds old
-            force_transport = self.bb.time_ms() - self.last_transport_time >= 10000
-
-        if (len(self.barn) > 0 and force_transport):
-            logger.info("Filling the barn took %s ms for [%s] to endpoint %s", self.total_harvest_time_ms, self.device.get_SN(), endpoints)
-            for endpoint in endpoints:
-
-                headers = self._create_headers(self.device)
-
-                transport = self.transport_factory(event_time + 100, self.bb, self.barn, headers)
-                transport.post_url = endpoint
-                ret.append(transport)
-
-                self.packet_count += 1
-                self.data_points_count += len(self.barn)
-
-            if self.bb.time_ms() >= self.packet_counter_timestamp + 60000:
-                logger.info("A total of [%s] data points were harvested from device [%s] in the last minute and [%s] packets were sent", self.data_points_count, self.device.get_SN(), self.packet_count)
-                self.packet_counter_timestamp = self.bb.time_ms()
-                self.packet_count = 0
-                self.data_points_count = 0
-
-            self.last_transport_time = self.bb.time_ms()
-            self.barn = {}
-            self.total_harvest_time_ms = 0
-
-        return ret
--- server/tasks/powerLimitControllerTask.py
+++ server/tasks/powerLimitControllerTask.py
@@ -1,21 +1,23 @@
 import logging
+from typing import List, Union
 from server.app.blackboard import BlackBoard
 from server.devices.Device import Device, DeviceMode, DeviceCommand, DeviceCommandType
 from server.tasks.harvest import Harvest
 from server.tasks.harvestTransport import DefaultHarvestTransportFactory
-from .task import Task
+from server.tasks.itask import ITask
+from .harvestableTask import HarvestableTask
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-class PowerLimitControllerTask(Task):
-    def __init__(self, event_time: int, bb: BlackBoard, device:Device):
-        super().__init__(event_time, bb)
-        self.device: Device = device
+class PowerLimitControllerTask(HarvestableTask):
+    def __init__(self, event_time: int, bb: BlackBoard, device: Device):
+        super().__init__(event_time, bb, device)
         self.is_initialized = False
 
-    def execute(self, event_time):
+    def execute(self, event_time) -> Union[List[ITask], ITask, None]:
+        start_time = event_time
 
         if self.device is None:
             logger.error(f"Device not found: {self.device.get_SN()}")
@@ -29,8 +31,10 @@ class PowerLimitControllerTask(Task):
             else:
                 logging.error(f"Failed to initialize device {self.device.get_SN()}")
 
-        # harvest data
-        self.device.read_harvest_data(force_verbose=False)
+        # harvest data (in control mode we do not want the mega harvest)
+        harvest = self.collect_harvest_data(start_time, force_verbose=False)
+        elapsed_time_ms = self.bb.time_ms() - start_time
+
         # TODO: check fuse protection
 
         if self.is_initialized:
@@ -38,7 +42,6 @@ class PowerLimitControllerTask(Task):
                 command:DeviceCommand = self.device.pop_command() # pop each command from the device
                 if command.command_type == DeviceCommandType.SET_BATTERY_POWER:
                     self.device.profile.set_battery_power(self.device, command.values[0])
-
 
         # deinit check here
         if self.device.get_mode() == DeviceMode.READ:
@@ -51,9 +54,17 @@ class PowerLimitControllerTask(Task):
 
             logging.info(f"Device {self.device.get_SN()} is in read mode, go to harvest")
 
-            return Harvest(event_time, self.bb, self.device, DefaultHarvestTransportFactory())
+            # Create final transport before switching to harvest mode
+            transports = self.create_final_transport(event_time, self.bb.settings.harvest.endpoints)
+            harvest_task = Harvest(event_time, self.bb, self.device, DefaultHarvestTransportFactory())
+            
+            return [harvest_task] + transports
 
+        # Check if it's time to transport the harvest
+        transport = self._create_transport(self.bb.time_ms() + elapsed_time_ms * 2, self.bb.settings.harvest.endpoints)
+        
         self.time = self.bb.time_ms() + 500
+        
+        if len(transport) > 0:
+            return [self] + transport
         return self
-
-        # logic function
