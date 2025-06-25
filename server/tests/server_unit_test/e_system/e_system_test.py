26ad2bf4ba881ba0c4fb987357d1090db7a38f65
h0bb3 <tobias.ohlsson@gmail.com>
2025-06-25 13:49:29 +0200

    better test

--- server/tests/server_unit_test/e_system/e_system_test.py
+++ server/tests/server_unit_test/e_system/e_system_test.py
@@ -1,15 +1,17 @@
 from server.e_system.e_system import ESystem
-from server.e_system.types import EBatteryType, EPower
+from server.e_system.types import EBatteryType, EPower, ELoadType
 import pytest
 
 
 @pytest.fixture
 def ebattery():
-    return EBatteryType(device_sn="test", power=EPower(value=-100), timestamp_ms=0) # negative power means battery is discharging
+    return EBatteryType(device_sn="test", power=EPower(value=-200), timestamp_ms=0) # negative power means battery is discharging
 
+@pytest.fixture
+def eload():
+    return ELoadType(device_sn="test", power=EPower(value=100), timestamp_ms=0) # positive power means load is consuming power (as is always should be)
 
-
-def test_self_consumption_battery(ebattery):
-    esystem = ESystem(battery_sns=[ebattery.device_sn], solar_sns=[], load_sns=[], grid_sns=[], parts=[ebattery])
+def test_self_consumption_battery(ebattery, eload):
+    esystem = ESystem(battery_sns=[ebattery.device_sn], solar_sns=[], load_sns=[eload.device_sn], grid_sns=[], parts=[ebattery, eload])
     esystem = esystem.self_consumption_battery()
-    assert esystem.get_battery_power().value == 0 
+    assert esystem.get_battery_power().value == -100 
