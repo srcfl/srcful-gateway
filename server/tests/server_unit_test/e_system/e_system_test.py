a168095d81ee9e27383daf0f3c97e38d8a4e3c4d
h0bb3 <tobias.ohlsson@gmail.com>
2025-06-27 08:57:47 +0200

    some testing changes

--- server/tests/server_unit_test/e_system/e_system_test.py
+++ server/tests/server_unit_test/e_system/e_system_test.py
@@ -13,5 +13,6 @@ def eload():
 
 def test_self_consumption_battery(ebattery, eload):
     esystem = ESystem(battery_sns=[ebattery.device_sn], solar_sns=[], load_sns=[eload.device_sn], grid_sns=[], parts=[ebattery, eload])
-    esystem = esystem.self_consumption_battery()
-    assert esystem.get_battery_power().value == -100 
+    new_esystem = esystem.self_consumption_battery()
+    assert new_esystem.get_battery_power().value == -100 # battery is now discharging 100W
+    assert esystem.get_battery_power().value == -200 # should be immutable
