45ebee7964b3c7017e61257a8f745cc08b917e18
David M <dmozartandraws@gmail.com>
2025-06-27 08:20:48 +0200

    add simple checks before creating non-existing types

--- server/devices/supported_devices/inverter_definitions/deye/deye.py
+++ server/devices/supported_devices/inverter_definitions/deye/deye.py
@@ -15,7 +15,7 @@ from server.e_system.types import (
 )
 from ...profile import ModbusProfile
 from ....common.types import ModbusDevice
-from typing import List
+from typing import List, Optional
 from server.devices.supported_devices.profile import RegisterInterval
 from .deye_legacy_profile import deye_legacy_profile
 
@@ -56,7 +56,7 @@ class DeyeProfile(ModbusProfile):
             TOTAL_PV_ENERGY=EEnergy(value=PV_TOTAL_ENERGY.decoded_value),
         )
 
-    def _get_battery(self, device_sn: str, timestamp_ms: int, decoded_registers: List[RegisterInterval], hv_lv_power_sf: float, hv_lv_voltage_sf: float) -> EBatteryType:
+    def _get_battery(self, device_sn: str, timestamp_ms: int, decoded_registers: List[RegisterInterval], hv_lv_power_sf: float, hv_lv_voltage_sf: float) -> Optional[EBatteryType]:
         BATT_POWER = self.get_register_interval(590, decoded_registers)
         BATTERY_CURRENT = self.get_register_interval(591, decoded_registers)
         BATTERY_VOLTAGE = self.get_register_interval(587, decoded_registers)
@@ -68,20 +68,23 @@ class DeyeProfile(ModbusProfile):
         BATT_TOTAL_CHARGE_ENERGY = self.get_register_interval(516, decoded_registers)
         BATT_TOTAL_DISCHARGE_ENERGY = self.get_register_interval(518, decoded_registers)
 
-        return EBatteryType(
-            device_sn=device_sn,
-            timestamp_ms=timestamp_ms,
-            POWER=EPower(value=BATT_POWER.decoded_value * hv_lv_power_sf),
-            CURRENT=ECurrent(value=BATTERY_CURRENT.decoded_value),
-            VOLTAGE=EVoltage(value=BATTERY_VOLTAGE.decoded_value * hv_lv_voltage_sf),
-            SOC=EPercent(value=BATTERY_SOC.decoded_value),
-            CAPACITY=EEnergy(value=BATTERY_CAPACITY.decoded_value),
-            TEMPERATURE=ETemperature(value=BATT_TEMPERATURE.decoded_value),
-            TOTAL_CHARGE_ENERGY=EEnergy(value=BATT_TOTAL_CHARGE_ENERGY.decoded_value),
-            TOTAL_DISCHARGE_ENERGY=EEnergy(value=BATT_TOTAL_DISCHARGE_ENERGY.decoded_value),
-        )
+        if BATTERY_SOC.decoded_value and 0 < BATTERY_SOC.decoded_value <= 100:
+            return EBatteryType(
+                device_sn=device_sn,
+                timestamp_ms=timestamp_ms,
+                POWER=EPower(value=BATT_POWER.decoded_value * hv_lv_power_sf),
+                CURRENT=ECurrent(value=BATTERY_CURRENT.decoded_value),
+                VOLTAGE=EVoltage(value=BATTERY_VOLTAGE.decoded_value * hv_lv_voltage_sf),
+                SOC=EPercent(value=BATTERY_SOC.decoded_value),
+                CAPACITY=EEnergy(value=BATTERY_CAPACITY.decoded_value),
+                TEMPERATURE=ETemperature(value=BATT_TEMPERATURE.decoded_value),
+                TOTAL_CHARGE_ENERGY=EEnergy(value=BATT_TOTAL_CHARGE_ENERGY.decoded_value),
+                TOTAL_DISCHARGE_ENERGY=EEnergy(value=BATT_TOTAL_DISCHARGE_ENERGY.decoded_value),
+            )
+        else:
+            return None
 
-    def _get_grid(self, device_sn: str, timestamp_ms: int, decoded_registers: List[RegisterInterval]) -> EGridType:
+    def _get_grid(self, device_sn: str, timestamp_ms: int, decoded_registers: List[RegisterInterval]) -> Optional[EGridType]:
         L1_CURRENT = self.get_register_interval(610, decoded_registers)
         L2_CURRENT = self.get_register_interval(611, decoded_registers)
         L3_CURRENT = self.get_register_interval(612, decoded_registers)
@@ -95,22 +98,25 @@ class DeyeProfile(ModbusProfile):
         TOTAL_IMPORT_ENERGY = self.get_register_interval(522, decoded_registers)
         TOTAL_EXPORT_ENERGY = self.get_register_interval(524, decoded_registers)
 
-        return EGridType(
-            device_sn=device_sn,
-            timestamp_ms=timestamp_ms,
-            L1_A=ECurrent(value=L1_CURRENT.decoded_value),
-            L2_A=ECurrent(value=L2_CURRENT.decoded_value),
-            L3_A=ECurrent(value=L3_CURRENT.decoded_value),
-            L1_V=EVoltage(value=L1_VOLTAGE.decoded_value),
-            L2_V=EVoltage(value=L2_VOLTAGE.decoded_value),
-            L3_V=EVoltage(value=L3_VOLTAGE.decoded_value),
-            L1_P=EPower(value=L1_POWER.decoded_value),
-            L2_P=EPower(value=L2_POWER.decoded_value),
-            L3_P=EPower(value=L3_POWER.decoded_value),
-            GRID_FREQUENCY=EFrequency(value=GRID_FREQUENCY.decoded_value),
-            TOTAL_IMPORT_ENERGY=EEnergy(value=TOTAL_IMPORT_ENERGY.decoded_value),
-            TOTAL_EXPORT_ENERGY=EEnergy(value=TOTAL_EXPORT_ENERGY.decoded_value),
-        )
+        if TOTAL_IMPORT_ENERGY.decoded_value > 0:
+            return EGridType(
+                device_sn=device_sn,
+                timestamp_ms=timestamp_ms,
+                L1_A=ECurrent(value=L1_CURRENT.decoded_value),
+                L2_A=ECurrent(value=L2_CURRENT.decoded_value),
+                L3_A=ECurrent(value=L3_CURRENT.decoded_value),
+                L1_V=EVoltage(value=L1_VOLTAGE.decoded_value),
+                L2_V=EVoltage(value=L2_VOLTAGE.decoded_value),
+                L3_V=EVoltage(value=L3_VOLTAGE.decoded_value),
+                L1_P=EPower(value=L1_POWER.decoded_value),
+                L2_P=EPower(value=L2_POWER.decoded_value),
+                L3_P=EPower(value=L3_POWER.decoded_value),
+                GRID_FREQUENCY=EFrequency(value=GRID_FREQUENCY.decoded_value),
+                TOTAL_IMPORT_ENERGY=EEnergy(value=TOTAL_IMPORT_ENERGY.decoded_value),
+                TOTAL_EXPORT_ENERGY=EEnergy(value=TOTAL_EXPORT_ENERGY.decoded_value),
+            )
+        else:
+            return None
 
     def _get_load(self, device_sn: str, timestamp_ms: int, decoded_registers: List[RegisterInterval]) -> ELoadType:
         LOAD_P = self.get_register_interval(653, decoded_registers)
@@ -131,10 +137,16 @@ class DeyeProfile(ModbusProfile):
         hv_lv_power_sf = 10 if HV_LV.decoded_value == 6 else 1
         hv_lv_voltage_sf = 0.1 if HV_LV.decoded_value == 6 else 0.01
 
-        esystem_data.append(self._get_metadata(device_sn, timestamp_ms, decoded_registers))
-        esystem_data.append(self._get_grid(device_sn, timestamp_ms, decoded_registers))
-        esystem_data.append(self._get_battery(device_sn, timestamp_ms, decoded_registers, hv_lv_power_sf, hv_lv_voltage_sf))
-        esystem_data.append(self._get_solar(device_sn, timestamp_ms, decoded_registers, hv_lv_power_sf))
-        esystem_data.append(self._get_load(device_sn, timestamp_ms, decoded_registers))
+        # Collect potential EType data objects
+        potential_data = [
+            self._get_metadata(device_sn, timestamp_ms, decoded_registers),
+            self._get_grid(device_sn, timestamp_ms, decoded_registers),
+            self._get_battery(device_sn, timestamp_ms, decoded_registers, hv_lv_power_sf, hv_lv_voltage_sf),
+            self._get_solar(device_sn, timestamp_ms, decoded_registers, hv_lv_power_sf),
+            self._get_load(device_sn, timestamp_ms, decoded_registers),
+        ]
+
+        # Append only the objects that were successfully created (i.e., not None)
+        esystem_data.extend([data for data in potential_data if data is not None])
 
         return esystem_data
